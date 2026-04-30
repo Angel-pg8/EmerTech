12c1f63661d37404069a01fbf9f49ea5b3189cda
Bardecuete121 <rr9631602@gmail.com>
2026-04-30 10:01:27 -0600

    cambio de imagen del welcome screen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import logo from "../assets/Logo.png"
+import logo from "../assets/loguito.png"
 import { saveUserProfile } from "../utils/userProfile"
 
 const initialForm = {
