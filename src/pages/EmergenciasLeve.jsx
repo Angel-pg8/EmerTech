8386a5fd2fb46e707cf81685846d21141636939b
Angel-pg8 <angel.hernandezgarcia2009@gmail.com>
2026-04-29 20:37:24 -0600

    rename de fotos

--- src/pages/EmergenciasLeve.jsx
+++ src/pages/EmergenciasLeve.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Logo from "../assets/loguito.png"
-import imgDeshidratacion from "../assets/Emergencias/Emergencias_leves_img/Deshidratacion.jpg"
-import imgDolorCabeza from "../assets/Emergencias/Emergencias_leves_img/dolor_cabeza.jpg"
-import imgDolorOido from "../assets/Emergencias/Emergencias_leves_img/dolor_oido.jpg"
-import imgIntoxicacion from "../assets/Emergencias/Emergencias_leves_img/intoxicacion.jpg"
-import imgQuemadura from "../assets/Emergencias/Emergencias_leves_img/quemadura_leve.jpg"
+import imgDeshidratacion from "../assets/Portada_emergencias/Emergencias_leves_img/Deshidratacion.jpg"
+import imgDolorCabeza from "../assets/Portada_emergencias/Emergencias_leves_img/dolor_cabeza.jpg"
+import imgDolorOido from "../assets/Portada_emergencias/Emergencias_leves_img/dolor_oido.jpg"
+import imgIntoxicacion from "../assets/Portada_emergencias/Emergencias_leves_img/intoxicacion.jpg"
+import imgQuemadura from "../assets/Portada_emergencias/Emergencias_leves_img/quemadura_leve.jpg"
 
  const emergencias = [
   {
